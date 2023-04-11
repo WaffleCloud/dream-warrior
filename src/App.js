2598dc010ea5b8e67732a6f7e60a4b90ddceac23
Rilo <taydh.wade@gmail.com>
2023-04-11 00:00:39 -0700

    added pictures, card design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import main from './main.jpg';
 import './App.css';
+import main from './main.jpg';
+// const shift = {
+//   name: "picture",
+//   image: require('./splash-img/shift.jpg')
+// }
+import shift from './splash-img/shift.jpg';
+import block from './splash-img/block.jpg';
+import look from './splash-img/look-up.jpg';
+
 
 function App() {
   return (
     <div className="container">
+
       <div className="intro"> 
-      <h1>Christian Jimenez</h1>
-      <p>Film Actor - Martial Artist - Voice Actor</p>
+      <h1>Christian Jimenez</h1> 
+      <h2>Film Actor - Martial Artist - Voice Actor</h2>
       </div>
-      <img src={main} className="main-img" alt="main-img" />
-      <div id="bio" className="card">Bio</div>
-      <div id="gallery" className="card">Gallery</div>
-      <div id="resume" className="card">Contact</div>
+      <div className="main-img-container"><img src={main} className="main-img" alt="main-img" /></div>
+      <div id="bio" className="card"> <img src={shift} alt="shift" className="splash-img" id="left"/>Bio and Resume</div>
+      <div id="gallery" className="card-center">Gallery<img src={look} alt="look" className="splash-img-right" /></div>
+      <div id="resume" className="card"><img src={block} alt="block" className="splash-img" id="left"/>Contact</div>
       <div class="contact"></div>
 
     </div>
