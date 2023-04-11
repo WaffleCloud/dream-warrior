66ebedd7e5999c4ca887b03fa9330893255f17c4
Rilo <taydh.wade@gmail.com>
2023-04-10 19:34:32 -0700

    added main img in src folder, trying to debug intro container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import main from './main.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-            <div id="bio" className="card">Bio</div>
-            <div id="gallery" className="card">Gallery</div>
-            <div id="resume" className="card">Contact</div>
-            <div class="contact"></div>
+      <div className="intro"> 
+      <h1>Christian Jimenez</h1>
+      <p>Film Actor - Martial Artist - Voice Actor</p>
+      </div>
+      <img src={main} className="main-img" alt="main-img" />
+      <div id="bio" className="card">Bio</div>
+      <div id="gallery" className="card">Gallery</div>
+      <div id="resume" className="card">Contact</div>
+      <div class="contact"></div>
+
     </div>
   );
 }
