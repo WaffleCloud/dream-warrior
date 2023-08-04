01247b18a69247d2d5e994453932bc0e263a08e0
Rilo <taydh.wade@gmail.com>
2023-08-04 14:43:29 -0700

    modified craco config

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    
-	webpack: {
-		configure: {
-			resolve: {
-				symlinks: false,
-			},
-		},
-	},
-};
+    webpack: {
+      configure: webpackConfig => {
+        const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+          ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
+        );
+  
+        webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+        return webpackConfig;
+      }
+    }
+  };
